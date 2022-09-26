bc6659b5a374dd939dcc7a14726be80a2104f77b
Trevor <trevorperry143@gmail.com>
2022-09-26 07:59:46 -0500

    commit tree 0f563ef5cf6a61c3abdcdb10a859ff9e0ea8b42f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+const personalData = {
+    name: `${name}`,
+    email: `${email}`
+}
+const dataRequest = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(personalData)
+}
+
+
+fetch("http://localhost:3000/users", dataRequest)
+.then(res => res.json())
+.then(json => (json))
+}
+submitData("Sam", "sam@sam.com")
